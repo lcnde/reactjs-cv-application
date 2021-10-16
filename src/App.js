8569698c2381ca40a089caeaef03bca5c656f529
lcnde <176673026+lcnde@users.noreply.github.com>
2021-10-16 06:53:34 +0200

    feature: added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './styles/App.css';
+import './styles/App.scss';
+import odinLogo from './assets/logo.png';
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <div className="header">
+        <img className="logo" src={odinLogo} alt="Logo of The Odin Project" />
+        <h1 className="title">CV APPLICATION</h1>
+      </div>
+      <form>
+        <div className=""></div>
+      </form>
     </div>
   );
 }
